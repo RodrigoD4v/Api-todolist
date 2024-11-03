4019f797507ee15bf65602241c0d8a0e8d874b93
RodrigoD4v <rodrigo20brbr@gmail.com>
2024-11-03 19:21:32 -0300

    Feat - Criar TaskController para gerenciamento de tarefas

--- src/main/java/com/reclamala/todolist/controller/TaskController.java
+++ src/main/java/com/reclamala/todolist/controller/TaskController.java
@@ -1,4 +1,24 @@
 package com.reclamala.todolist.controller;
 
+import com.reclamala.todolist.models.Task;
+import com.reclamala.todolist.service.FirestoreService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/tasks")
 public class TaskController {
+
+    @Autowired
+    private FirestoreService firestoreService;
+
+    @PostMapping
+    public String createTask(@RequestBody Task task) {
+        try {
+            return firestoreService.saveTask(task);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return "Error creating task: " + e.getMessage();
+        }
+    }
 }
