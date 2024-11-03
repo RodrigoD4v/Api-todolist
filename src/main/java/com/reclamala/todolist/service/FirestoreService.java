9dd728de8d72360b5abd444db9e3d029e44729b9
RodrigoD4v <rodrigo20brbr@gmail.com>
2024-11-03 19:16:51 -0300

    Feat - Criando service de teste para salvar uma task

--- src/main/java/com/reclamala/todolist/service/FirestoreService.java
+++ src/main/java/com/reclamala/todolist/service/FirestoreService.java
@@ -1,5 +1,22 @@
 package com.reclamala.todolist.service;
 
+import com.google.api.core.ApiFuture;
+import com.google.cloud.firestore.CollectionReference;
+import com.google.cloud.firestore.Firestore;
+import com.google.cloud.firestore.WriteResult;
+import com.reclamala.todolist.models.Task;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class FirestoreService {
 
+    @Autowired
+    private Firestore firestore;
+
+    public String saveTask(Task task) throws Exception {
+        CollectionReference tasks = firestore.collection("tasks");
+        ApiFuture<WriteResult> writeResult = tasks.document(task.getId()).set(task);
+        return writeResult.get().getUpdateTime().toString();
+    }
 }
