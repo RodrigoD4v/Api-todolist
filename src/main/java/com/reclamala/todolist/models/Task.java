020141088dbea9bf962572ef9502ede0fa55c978
RodrigoD4v <rodrigo20brbr@gmail.com>
2024-11-18 04:55:37 -0300

    Refactor - Aprimoramento da classe das Tasks sem mudar a funcionalidade

--- src/main/java/com/reclamala/todolist/models/Task.java
+++ src/main/java/com/reclamala/todolist/models/Task.java
@@ -4,28 +4,44 @@ public class Task {
     private String id;
     private String title;
     private String description;
-
+    private String createdAt;
+    private String updatedAt;
+    private boolean completed;
+    
     public String getId() {
         return id;
     }
-
     public void setId(String id) {
         this.id = id;
     }
-
     public String getTitle() {
         return title;
     }
-
     public void setTitle(String title) {
         this.title = title;
     }
-
     public String getDescription() {
         return description;
     }
-
     public void setDescription(String description) {
         this.description = description;
+    }
+    public String getCreatedAt() {
+        return createdAt;
+    }
+    public void setCreatedAt(String createdAt) {
+        this.createdAt = createdAt;
+    }
+    public String getUpdatedAt() {
+        return updatedAt;
+    }
+    public void setUpdatedAt(String updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+    public boolean isCompleted() {
+        return completed;
+    }
+    public void setCompleted(boolean completed) {
+        this.completed = completed;
     }
 }
