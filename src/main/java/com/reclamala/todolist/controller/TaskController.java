352ebc7bf0e48400ed3836958a227de5e0cf7d6f
RodrigoD4v <rodrigo20brbr@gmail.com>
2024-11-18 04:52:22 -0300

    Refactor:recycle:- Melhoria e mudança no formado de como é processado o salvamento da task no firestore

--- src/main/java/com/reclamala/todolist/controller/TaskController.java
+++ src/main/java/com/reclamala/todolist/controller/TaskController.java
@@ -3,6 +3,8 @@ package com.reclamala.todolist.controller;
 import com.reclamala.todolist.models.Task;
 import com.reclamala.todolist.service.FirestoreService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -12,13 +14,19 @@ public class TaskController {
     @Autowired
     private FirestoreService firestoreService;
 
-    @PostMapping
-    public String createTask(@RequestBody Task task) {
+    @PostMapping("/{userUid}")
+    public ResponseEntity<String> createTask(@PathVariable String userUid, @RequestBody Task task) {
         try {
-            return firestoreService.saveTask(task);
+            // Printando o conteúdo da tarefa recebida para visualizar no terminal
+            System.out.println("Dados recebidos para a tarefa: " + task);
+            System.out.println("ID do usuário: " + userUid);
+
+            // Salvar tarefa no Firestore para o usuário específico
+            String result = firestoreService.saveTask(userUid, task);
+            return ResponseEntity.ok("Tarefa criada com sucesso: " + result);
         } catch (Exception e) {
-            e.printStackTrace();
-            return "Error creating task: " + e.getMessage();
+            System.out.println("Erro na criação da tarefa: " + e.getMessage());
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Erro ao criar tarefa");
         }
     }
 }
