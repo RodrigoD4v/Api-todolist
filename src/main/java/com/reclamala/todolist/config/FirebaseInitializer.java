e9e0bf5df97e0d9c66b1b2e1c1492b4947cb5055
RodrigoD4v <rodrigo20brbr@gmail.com>
2024-11-21 14:28:32 -0300

    Voltando ao original

--- src/main/java/com/reclamala/todolist/config/FirebaseInitializer.java
+++ src/main/java/com/reclamala/todolist/config/FirebaseInitializer.java
@@ -14,29 +14,23 @@ import java.io.IOException;
 @Configuration
 public class FirebaseInitializer {
 
-    @Bean
-    public Firestore firestore() throws IOException {
-        String serviceAccountPath = System.getenv("FIREBASE_KEY");
-
-        if (serviceAccountPath == null || serviceAccountPath.isEmpty()) {
-            throw new IllegalArgumentException("FIREBASE_KEY não está configurado nas variáveis de ambiente do Railway.");
-        }
-
-        FileInputStream serviceAccount = new FileInputStream(serviceAccountPath);
-
-        FirebaseOptions options = new FirebaseOptions.Builder()
-                .setCredentials(GoogleCredentials.fromStream(serviceAccount))
-                .build();
-
-        // Inicialize o Firebase App se ainda não foi inicializado
-        if (FirebaseApp.getApps().isEmpty()) {
-            FirebaseApp.initializeApp(options);
-            System.out.println("Firebase App initialized successfully.");
-        }
-
-        // Inicialize o Firestore
-        Firestore firestore = FirestoreClient.getFirestore();
-        System.out.println("Firestore initialized successfully.");
-        return firestore;
-    }
+  @Bean
+  public Firestore firestore() throws IOException {
+      FileInputStream serviceAccount =
+              new FileInputStream("src/main/resources/serviceAccountKey.json");
+  
+      FirebaseOptions options = new FirebaseOptions.Builder()
+              .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+              .build();
+  
+      if (FirebaseApp.getApps().isEmpty()) {
+          FirebaseApp.initializeApp(options);
+          System.out.println("Firebase App initialized successfully.");
+      }
+  
+      Firestore firestore = FirestoreClient.getFirestore();
+      System.out.println("Firestore initialized successfully.");
+      return firestore;
+  }
+  
 }
