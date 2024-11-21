bde2e7bc7dc2d069e921602f5e4cf858d7a0adca
RodrigoD4v <rodrigo20brbr@gmail.com>
2024-11-21 14:00:38 -0300

    Mudando forma de inicialização do firebase

--- src/main/java/com/reclamala/todolist/config/FirebaseInitializer.java
+++ src/main/java/com/reclamala/todolist/config/FirebaseInitializer.java
@@ -5,6 +5,7 @@ import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
 import com.google.firebase.cloud.FirestoreClient;
 import com.google.cloud.firestore.Firestore;
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -14,23 +15,30 @@ import java.io.IOException;
 @Configuration
 public class FirebaseInitializer {
 
-  @Bean
-  public Firestore firestore() throws IOException {
-      FileInputStream serviceAccount =
-              new FileInputStream("src/main/resources/serviceAccountKey.json");
-  
-      FirebaseOptions options = new FirebaseOptions.Builder()
-              .setCredentials(GoogleCredentials.fromStream(serviceAccount))
-              .build();
-  
-      if (FirebaseApp.getApps().isEmpty()) {
-          FirebaseApp.initializeApp(options);
-          System.out.println("Firebase App initialized successfully.");
-      }
-  
-      Firestore firestore = FirestoreClient.getFirestore();
-      System.out.println("Firestore initialized successfully.");
-      return firestore;
-  }
-  
+    @Bean
+    public Firestore firestore() throws IOException {
+
+        Dotenv dotenv = Dotenv.load();
+        
+        String serviceAccountPath = dotenv.get("FIREBASE_KEY");
+
+        if (serviceAccountPath == null || serviceAccountPath.isEmpty()) {
+            throw new IllegalArgumentException("FIREBASE_KEY não está configurado no .env.");
+        }
+
+        FileInputStream serviceAccount = new FileInputStream(serviceAccountPath);
+
+        FirebaseOptions options = new FirebaseOptions.Builder()
+                .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+                .build();
+
+        if (FirebaseApp.getApps().isEmpty()) {
+            FirebaseApp.initializeApp(options);
+            System.out.println("Firebase App initialized successfully.");
+        }
+
+        Firestore firestore = FirestoreClient.getFirestore();
+        System.out.println("Firestore initialized successfully.");
+        return firestore;
+    }
 }
